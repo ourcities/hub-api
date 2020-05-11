e2a0d8b6534882e6ea57a8507de24964bd2fbbf6
Lucas Pirola <lucaspirola@gmail.com>
2020-05-11 18:40:56 -0300

    fix(migrations): Change widget settings to text

--- db/migrate/20150714192829_create_widgets.rb
+++ db/migrate/20150714192829_create_widgets.rb
@@ -4,7 +4,7 @@ class CreateWidgets < ActiveRecord::Migration
     create_table :widgets do |t|
       t.integer :block_id
       t.integer :size
-      t.hstore :settings
+      t.text :settings
       t.string :kind
 
       t.timestamps null: false
